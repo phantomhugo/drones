0b5f8e5313fb41183b1f7a084a3fd5ef51c85e6f
Hugo Castellanos <hacastellanosm@unal.edu.co>
2023-03-06 21:40:59 -0500

    feat: created base application

---
+++ src/test/java/com/hugo/drones/DronesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugo.drones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
